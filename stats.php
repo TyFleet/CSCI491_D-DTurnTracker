9947dd7501d1e53edd95796fdeca479be470702b
Tyler Gentry <tgentry2753@gmail.com>
2019-11-07 12:53:13 -0700

    Initial stats page

---
+++ stats.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang = "en">
+
+<head>
+    <meta charset="UTF=8">
+    <title>Character Stats Creator</title>
+</head>
+
+<body>
+    <h1>Stats Page</h1>
+
+<?php
+
+
+?>
+
+</body>
+</html>
