31bd6b0ab3bef26f7da3406999afe9b2b1d87244
Tyler Gentry <tgentry2753@gmail.com>
2019-11-12 17:37:35 -0700

    added more fields and required attributes

--- stats.php
+++ stats.php
@@ -5,8 +5,14 @@ echo <<<_END
     <fieldset id='char_sheet'>
         <legend>Character Creation</legend>
         
+        
+        <p>
+        <input type='radio' name='char_type' value='player' id='player' checked><label for='player'>Player</label>
+        <input type='radio' name='char_type' value='monster' id='monster'><label for='monster'>Monster</label>
+        </p>
+        
         <label for='char_name'>Character Name</label>
-        <input type='text id='char_name' name='char_name' placeholder='Llewyn'>
+        <input type='text id='char_name' name='char_name' placeholder='Llewyn' required>
         
         <label for='player_name'>Player Name</label>
         <input type='text' id='player_name' name='player_name' placeholder='John Smith'><br><br>
@@ -19,7 +25,7 @@ echo <<<_END
             <option value="druid">Druid</option>
             <option value="fighter">Fighter</option>
             <option value="monk">Monk</option>
-            <option value="paladin>Paladin</option>
+            <option value="paladin">Paladin</option>
             <option value="ranger">Ranger</option>
             <option value="rouge">Rouge</option>
             <option value="sorcerer">Sorcerer</option>
@@ -29,29 +35,29 @@ echo <<<_END
         <br><br>
         
         <label for='level'>Level</label>
-        <input type='number' id='level' name='level' placeholder='1'><br><br>
+        <input type='number' id='level' name='level' placeholder='1' required><br><br>
         
         <label for='hp'>HP</label>
-        <input type='number' id='hp' name='hp' placeholder='0'>
+        <input type='number' id='hp' name='hp' placeholder='0' required>
         
         <label for='armor'>Armor Class</label>
-        <input type='number' id='armor' name='armor placeholder='0'>
+        <input type='number' id='armor' name='armor placeholder='0' required>
         
         <label for='speed'>Speed</label>
-        <input type='text' id='speed' name='speed' placeholder='30'><br><br>
+        <input type='text' id='speed' name='speed' placeholder='30' required><br><br>
         
         <label for='strength'>Strength</label>
-        <input type='text' id='strength' name='strength' placeholder='0'>
+        <input type='text' id='strength' name='strength' placeholder='0' required>
         <label for='dex'>Dexterity</label>
-        <input type='text' id='dex' name='dex' placeholder='0'>
+        <input type='text' id='dex' name='dex' placeholder='0' required>
         <label for='constit'>Constitution</label>
-        <input type='text' id='constit' name='constit' placeholder='0'><br><br>
+        <input type='text' id='constit' name='constit' placeholder='0' required><br><br>
         <label for='intel'>Intelligence</label>
-        <input type='text' id='intel' name='intel' placeholder='0'>
+        <input type='text' id='intel' name='intel' placeholder='0' required>
         <label for='wisdom'>Wisdom</label>
-        <input type='text' id='wisdom' name='wisdom' placeholder='0'>
+        <input type='text' id='wisdom' name='wisdom' placeholder='0' required>
         <label for='charisma'>Charisma</label>
-        <input type='text' id='charisma' name='charisma' placeholder='0'><br><br>
+        <input type='text' id='charisma' name='charisma' placeholder='0' required><br><br>
         Character Image:
         <input type='file' name='image' size='14'><br><br>
         
@@ -69,7 +75,7 @@ echo <<<_END
         </select>
         
         <label for='race'>Race</label>
-        <input type='text' id='race' name='race' placeholder='Human'><br><br>
+        <input type='text' id='race' name='race' placeholder='Human' required><br><br>
         
         <label for='profs'>Proficiencies</label>
         <br>
@@ -82,8 +88,6 @@ echo <<<_END
         <p>
             <input type="submit" value="Create Character">
         </p>
-        
-        
     </fieldset>
 _END;
 #require_once 'footer.php'
