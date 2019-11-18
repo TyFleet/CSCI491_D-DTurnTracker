56544dcfc2685c8a60d86d9c411a815ccb54365c
Tyler Gentry <tgentry2753@gmail.com>
2019-11-18 10:00:38 -0700

    added min/max contraints, divs to associate fields

--- stats.php
+++ stats.php
@@ -1,22 +1,32 @@
+<!DOCTYPE html>
+<html>
+    
+    <head>
+        <meta charset='utf-8'>
+        <link rel='stylesheet' href='css/styles.css'>
+        <title>Character Creator</title>
+    </head>
 <?php
 #require_once 'header.php';
 echo <<<_END
-<form action='https://wp.zybooks.com/form-viewer.php' target='_blank' method='POST'>
+<form action='https://wp.zybooks.com/form-viewer.php' target='_blank' method='POST' id='character_form'>
     <fieldset id='char_sheet'>
         <legend>Character Creation</legend>
         
         
-        <p>
+        <div>
         <input type='radio' name='char_type' value='player' id='player' checked><label for='player'>Player</label>
         <input type='radio' name='char_type' value='monster' id='monster'><label for='monster'>Monster</label>
-        </p>
+        </div>
         
+        <div>
         <label for='char_name'>Character Name</label>
         <input type='text id='char_name' name='char_name' placeholder='Llewyn' required>
-        
         <label for='player_name'>Player Name</label>
-        <input type='text' id='player_name' name='player_name' placeholder='John Smith'><br><br>
+        <input type='text' id='player_name' name='player_name' placeholder='John Smith'>
+        </div>
         
+        <div>
         <label for='class'>Class</label>
         <select id='class' name="class">
             <option value="barbarian">Barbarian</option>
@@ -32,35 +42,62 @@ echo <<<_END
             <option value="warlock">Warlock</option>
             <option value="wizard">Wizard</option>
         </select>
+        
+        <label for='monster_type'>Monster Type</label>
+        <select id='monster_type' name="monster_type">
+            <option value="aberration">Aberration</option>
+            <option value="beast">Beast</option>
+            <option value="celestial">Celestial</option>
+            <option value="construct">Construct</option>
+            <option value="dragon">Elemental</option>
+            <option value="elemental">Elemental</option>
+            <option value="fey">Fey</option>
+            <option value="fiend">Fiend</option>
+            <option value="giant">Giant</option>
+            <option value="humanoid">Humanoid</option>
+            <option value="monstrosity">Monstrosity</option>
+            <option value="ooze">Ooze</option>
+            <option value="plant">Plant</option>
+            <option value="undead">Undead</option>
+        </select>
+        </div>
         <br><br>
         
+        <div>
         <label for='level'>Level</label>
-        <input type='number' id='level' name='level' placeholder='1' required><br><br>
+        <input type='number' id='level' name='level' placeholder='1' min='1' max='20' required><br><br>
         
         <label for='hp'>HP</label>
-        <input type='number' id='hp' name='hp' placeholder='0' required>
+        <input type='number' id='hp' name='hp' placeholder='0' min='0' required>
         
         <label for='armor'>Armor Class</label>
-        <input type='number' id='armor' name='armor' placeholder='0' required>
+        <input type='number' id='armor' name='armor' placeholder='0' min='0' required>
         
         <label for='speed'>Speed</label>
-        <input type='text' id='speed' name='speed' placeholder='30' required><br><br>
+        <input type='number' id='speed' name='speed' placeholder='30' min='0' required>
+        </div>
+        <br><br>
         
+        
+        <div>
         <label for='strength'>Strength</label>
-        <input type='text' id='strength' name='strength' placeholder='0' required>
+        <input type='number' id='strength' name='strength' placeholder='0' min='0' max ='20' required>
         <label for='dex'>Dexterity</label>
-        <input type='text' id='dex' name='dex' placeholder='0' required>
+        <input type='number' id='dex' name='dex' placeholder='0' min='0' max ='20' required>
         <label for='constit'>Constitution</label>
-        <input type='text' id='constit' name='constit' placeholder='0' required><br><br>
+        <input type='number' id='constit' name='constit' placeholder='0' min='0' max ='20' required><br><br>
         <label for='intel'>Intelligence</label>
-        <input type='text' id='intel' name='intel' placeholder='0' required>
+        <input type='number' id='intel' name='intel' placeholder='0' min='0' max ='20' required>
         <label for='wisdom'>Wisdom</label>
-        <input type='text' id='wisdom' name='wisdom' placeholder='0' required>
+        <input type='number' id='wisdom' name='wisdom' placeholder='0' min='0' max ='20' required>
         <label for='charisma'>Charisma</label>
-        <input type='text' id='charisma' name='charisma' placeholder='0' required><br><br>
+        <input type='number' id='charisma' name='charisma' placeholder='0' min='0' max ='20' required>
+        </div><br><br>
+        
         Character Image:
         <input type='file' name='image' size='14'><br><br>
         
+        <div>
         <label for='align'>Alignment</label>
         <select id='align' name="align">
             <option value="lawG">Lawful Good</option>
@@ -75,21 +112,34 @@ echo <<<_END
         </select>
         
         <label for='race'>Race</label>
-        <input type='text' id='race' name='race' placeholder='Human' required><br><br>
+        <input type='text' id='race' name='race' placeholder='Human' required>
+        </div><br><br>
         
+        <div>
         <label for='profs'>Proficiencies</label>
         <br>
-        <textarea rows='5' cols='20' id='profs' name='profs'></textarea><br>
+        <textarea rows='7' cols='40' id='profs' name='profs'></textarea><br>
         
         <label for='notes'>Notes</label>
         <br>
-        <textarea rows="5" cols="20" id='notes' name='notes'></textarea>
+        <textarea rows="7" cols="40" id='notes' name='notes'></textarea>
+        </div>
         
         <p>
             <input type="submit" value="Create Character">
         </p>
     </fieldset>
 _END;
+
+echo <<<_END
+    <div id='creation_help'>
+        <p>
+            This is where the info pulled from the DB will go to assist in character creation.
+        </p>
+    </div>
+_END;
 #require_once 'footer.php'
 ?>
+</html>
+
 
