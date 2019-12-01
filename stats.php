4fd868bb4dae9a6eb25d1ba7dfc9c0577fe4a91d
Kyle Hagerman <hagermankyle96@gmail.com>
2019-11-30 22:29:30 -0700

    Added sql statement action to stats.php form

--- stats.php
+++ stats.php
@@ -1,22 +1,21 @@
 <?php
-#require_once 'header.php';
+require_once 'functions.php';
 echo <<<_END
-<form action='https://wp.zybooks.com/form-viewer.php' target='_blank' method='POST'>
+<form action='createChar.php' target='_self' method='POST'>
     <fieldset id='char_sheet'>
         <legend>Character Creation</legend>
-        
-        
+
         <p>
         <input type='radio' name='char_type' value='player' id='player' checked><label for='player'>Player</label>
         <input type='radio' name='char_type' value='monster' id='monster'><label for='monster'>Monster</label>
         </p>
-        
+
         <label for='char_name'>Character Name</label>
         <input type='text id='char_name' name='char_name' placeholder='Llewyn' required>
-        
+
         <label for='player_name'>Player Name</label>
         <input type='text' id='player_name' name='player_name' placeholder='John Smith'><br><br>
-        
+
         <label for='class'>Class</label>
         <select id='class' name="class">
             <option value="barbarian">Barbarian</option>
@@ -33,19 +32,19 @@ echo <<<_END
             <option value="wizard">Wizard</option>
         </select>
         <br><br>
-        
+
         <label for='level'>Level</label>
         <input type='number' id='level' name='level' placeholder='1' required><br><br>
-        
+
         <label for='hp'>HP</label>
         <input type='number' id='hp' name='hp' placeholder='0' required>
-        
+
         <label for='armor'>Armor Class</label>
         <input type='number' id='armor' name='armor' placeholder='0' required>
-        
+
         <label for='speed'>Speed</label>
         <input type='text' id='speed' name='speed' placeholder='30' required><br><br>
-        
+
         <label for='strength'>Strength</label>
         <input type='text' id='strength' name='strength' placeholder='0' required>
         <label for='dex'>Dexterity</label>
@@ -60,7 +59,7 @@ echo <<<_END
         <input type='text' id='charisma' name='charisma' placeholder='0' required><br><br>
         Character Image:
         <input type='file' name='image' size='14'><br><br>
-        
+
         <label for='align'>Alignment</label>
         <select id='align' name="align">
             <option value="lawG">Lawful Good</option>
@@ -73,18 +72,18 @@ echo <<<_END
             <option value="neutE">Neutral Evil</option>
             <option value="chaE">Chaotic Evil</option>
         </select>
-        
+
         <label for='race'>Race</label>
         <input type='text' id='race' name='race' placeholder='Human' required><br><br>
-        
+
         <label for='profs'>Proficiencies</label>
         <br>
-        <textarea rows='5' cols='20' id='profs' name='profs'></textarea><br>
-        
+        <textarea rows='5' cols='50' id='profs' name='profs'></textarea><br>
+
         <label for='notes'>Notes</label>
         <br>
-        <textarea rows="5" cols="20" id='notes' name='notes'></textarea>
-        
+        <textarea rows="5" cols="50" id='notes' name='notes'></textarea>
+
         <p>
             <input type="submit" value="Create Character">
         </p>
@@ -92,4 +91,3 @@ echo <<<_END
 _END;
 #require_once 'footer.php'
 ?>
-
