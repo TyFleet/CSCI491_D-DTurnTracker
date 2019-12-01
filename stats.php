0f58702a9a9e9b4f5f0300e9e2e6dd015d882005
Kyle Hagerman <hagermankyle96@gmail.com>
2019-12-01 12:21:56 -0700

    fixing query syntax in stats.php, I don't think I need a separate php file to process the form either

--- stats.php
+++ stats.php
@@ -1,7 +1,7 @@
 <?php
 require_once 'functions.php';
 echo <<<_END
-<form action='createChar.php' target='_self' method='POST'>
+<form action='stats.php' target='_self' method='POST'>
     <fieldset id='char_sheet'>
         <legend>Character Creation</legend>
 
@@ -89,5 +89,89 @@ echo <<<_END
         </p>
     </fieldset>
 _END;
+
+$insert_statement = "INSERT INTO characterInfo
+                                   (charName,
+                                   identifier,
+                                   class,
+                                   level,
+                                   background,
+                                   playerName,
+                                   race,
+                                   alignment,
+                                   strength,
+                                   dexterity,
+                                   constitution,
+                                   intelligence,
+                                   wisdom,
+                                   charisma,
+                                   armorclass,
+                                   initiative,
+                                   speed,
+                                   maxHP,
+                                   currentHP,
+                                   acrobatics,
+                                   animalHandling,
+                                   arcana,
+                                   athletics,
+                                   deception,
+                                   history,
+                                   insight,
+                                   intimidation,
+                                   investigation,
+                                   medicine,
+                                   nature,
+                                   perception,
+                                   performance,
+                                   persuasion,
+                                   religion,
+                                   sleight of hand,
+                                   stealth,
+                                   survival,
+                                   hitDice)
+                                   VALUES (
+                                     $_POST[`char_name`],
+                                     NULL,
+                                     $_POST['class'],
+                                     $_POST['level'],
+                                     $_POST[value-5],
+                                     NULL,
+                                     $_POST['player_name'],
+                                     $_POST['race'],
+                                     $_POST['align'],
+                                     $_POST['strength'],
+                                     $_POST['dex'],
+                                     $_POST['constit'],
+                                     $_POST['intel'],
+                                     $_POST['wisdom'],
+                                     $_POST['charisma'],
+                                     $_POST['armor'],
+                                     NULL,
+                                     $_POST['speed'],
+                                     $_POST['hp'],
+                                     $_POST['hp'],
+                                     NULL,
+                                     NULL,
+                                     NULL,
+                                     NULL,
+                                     NULL,
+                                     NULL,
+                                     NULL,
+                                     NULL,
+                                     NULL,
+                                     NULL,
+                                     NULL,
+                                     NULL,
+                                     NULL,
+                                     NULL,
+                                     NULL,
+                                     NULL,
+                                     NULL,
+                                     NULL,
+                                     $_POST['profs'],
+                                     $_POST['notes'],
+                                     $chartype)";
+
+queryMysql($insert_statement);
 #require_once 'footer.php'
 ?>
