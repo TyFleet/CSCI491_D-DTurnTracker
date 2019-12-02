3e90f047c0a979b99ba918c9cfc4a8b777fd1065
Kyle Hagerman <hagermankyle96@gmail.com>
2019-12-01 17:20:41 -0700

    still troubleshooting insert query

--- stats.php
+++ stats.php
@@ -100,86 +100,67 @@ if(isset($_POST['player_name'])){
     $chartype = 0;
   }
 
-  $insert_statement = "INSERT INTO characterInfo
-                                     (charName,
-                                     identifier,
-                                     class,
-                                     level,
-                                     background,
-                                     playerName,
-                                     race,
-                                     alignment,
-                                     strength,
-                                     dexterity,
-                                     constitution,
-                                     intelligence,
-                                     wisdom,
-                                     charisma,
-                                     armorclass,
-                                     initiative,
-                                     speed,
-                                     maxHP,
-                                     currentHP,
-                                     acrobatics,
-                                     animalHandling,
-                                     arcana,
-                                     athletics,
-                                     deception,
-                                     history,
-                                     insight,
-                                     intimidation,
-                                     investigation,
-                                     medicine,
-                                     nature,
-                                     perception,
-                                     performance,
-                                     persuasion,
-                                     religion,
-                                     sleight of hand,
-                                     stealth,
-                                     survival,
-                                     hitDice)
-                                     VALUES (".
-                                       $_POST['char_name'].",
-                                       NULL,".
-                                       $_POST['class'].",".
-                                       $_POST['level'].","
-                                       NULL,".
-                                       $_POST['player_name'].",".
-                                       $_POST['race'].",".
-                                       $_POST['align'].",".
-                                       $_POST['strength'].",".
-                                       $_POST['dex'].",".
-                                       $_POST['constit'].",".
-                                       $_POST['intel'].",".
-                                       $_POST['wisdom'].",".
-                                       $_POST['charisma'].",".
-                                       $_POST['armor'].",
-                                       NULL,".
-                                       $_POST['speed'].",".
-                                       $_POST['hp'].",".
-                                       $_POST['hp'].",
-                                       NULL,
-                                       NULL,
-                                       NULL,
-                                       NULL,
-                                       NULL,
-                                       NULL,
-                                       NULL,
-                                       NULL,
-                                       NULL,
-                                       NULL,
-                                       NULL,
-                                       NULL,
-                                       NULL,
-                                       NULL,
-                                       NULL,
-                                       NULL,
-                                       NULL,
-                                       NULL,".
-                                       $_POST['profs'].",".
-                                       $_POST['notes'].",".
-                                       $chartype.")";
+  $insert_statement = "INSERT INTO definitions (dexterity) VALUES ('This skill measures balance')";
+
+  // $insert_statement = "INSERT INTO characterInfo
+  //                                    (charName,
+  //                                    identifier,
+  //                                    class,
+  //                                    level,
+  //                                    background,
+  //                                    playerName,
+  //                                    race,
+  //                                    alignment,
+  //                                    strength,
+  //                                    dexterity,
+  //                                    constitution,
+  //                                    intelligence,
+  //                                    wisdom,
+  //                                    charisma,
+  //                                    armorclass,
+  //                                    initiative,
+  //                                    speed,
+  //                                    maxHP,
+  //                                    currentHP,
+  //                                    acrobatics,
+  //                                    animalHandling,
+  //                                    arcana,
+  //                                    athletics,
+  //                                    deception,
+  //                                    history,
+  //                                    insight,
+  //                                    intimidation,
+  //                                    investigation,
+  //                                    medicine,
+  //                                    nature,
+  //                                    perception,
+  //                                    performance,
+  //                                    persuasion,
+  //                                    religion,
+  //                                    sleight of hand,
+  //                                    stealth,
+  //                                    survival,
+  //                                    hitDice)
+  //                                    VALUES (".
+  //                                      $_POST['char_name'],
+  //                                      $_POST['class'],
+  //                                      $_POST['level'],
+  //                                      $_POST['player_name'],
+  //                                      $_POST['race'],
+  //                                      $_POST['align'],
+  //                                      $_POST['strength'],
+  //                                      $_POST['dex'],
+  //                                      $_POST['constit'],
+  //                                      $_POST['intel'],
+  //                                      $_POST['wisdom'],
+  //                                      $_POST['charisma'],
+  //                                      $_POST['armor'],
+  //                                      $_POST['speed'],
+  //                                      $_POST['hp'],
+  //                                      $_POST['hp'],
+  //                                      $_POST['profs'],
+  //                                      $_POST['notes'],
+  //                                      $chartype.")";
 
   queryMysql($insert_statement);
 }
